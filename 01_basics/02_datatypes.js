f1ae1d390424c5cfb4f57a5af6e22b107a3248e9
Siddharth chavda <79299514+siddharthchavda@users.noreply.github.com>
2024-12-14 10:52:11 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+console.log("siddharth");
+let name="sid"
+let age= 22 
+let isLoggingIn= true
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null =>standalone value
+// undefiend =>
+// symbol =>unique
+// object
+
+console.log(typeof name)
