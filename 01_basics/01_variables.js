70648f1facddc4059c832f9a461e78111ac2764c
Siddharth chavda <79299514+siddharthchavda@users.noreply.github.com>
2024-12-14 10:56:11 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12344
+let accountEmail="sid@google.com"
+var accountPassword="123456"
+accountCity="jaipur"
+let accountState;
+
+accountEmail="sidd@gmail.com"
+accountPassword="121212"
+accountCity="rajkot"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountState])
