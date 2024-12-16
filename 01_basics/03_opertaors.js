8f7f5b6f8abeaadb97945d32f1acc9572f75e9f7
siddharthchavda <sidchavda552@gmail.com>
2024-12-16 12:55:01 +0530

    opertaor

--- 01_basics/03_opertaors.js
+++ 01_basics/03_opertaors.js
@@ -1,7 +1,28 @@
-let score = 33
-console.log(score);
+// let score = 33
+// console.log(score);
 
-let someNumber =55
-let StringNumber= String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let someNumber =55
+// let Stri
+
+// ngNumber= String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+// ****************************operators *********************************//
+
+let value = 3
+let NegValue = -value 
+console.log(NegValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " siddharth"
+let str3 = str1+str2
+console.log(str3);
